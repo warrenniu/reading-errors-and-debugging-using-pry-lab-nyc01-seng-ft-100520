a77e6b57fade0102355ddcc1454959cf6b55a0a6
Warren Niu <warrenniu@gmail.com>
2020-08-31 19:33:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,11 +2,13 @@
 require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
 end
 
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
 end
 
 def engage
