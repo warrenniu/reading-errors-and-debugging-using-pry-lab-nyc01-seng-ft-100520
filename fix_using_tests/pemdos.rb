db28db7be6d06f50e59afaa56c7fc7612d4cccf7
Warren Niu <warrenniu@gmail.com>
2020-08-31 19:44:48 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    ("s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" "s" + "s") + string
   else
     string
   end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,18 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
+  binding.pry
 end
 
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
 end
 
 def engage
-  puts state_log(date)
   date = generate_star_date
+  puts state_log(date)
+  binding.pry
 end
