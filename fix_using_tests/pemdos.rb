cef9d126ec812c78012287dae5a4cb95e86b7b83
Warren Niu <warrenniu@gmail.com>
2020-08-31 19:30:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,7 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     binding.pry
-    ("s" ^ 10.to_s) + string
+    ("s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" "s" + "s") + string
   else
     string
   end
